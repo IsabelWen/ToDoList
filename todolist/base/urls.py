3c93beaa24be1dbf3b4fa649a73dd22a91cc91cd
Isy <isy1997@freenet.de>
2024-05-08 13:32:19 +0200

    Create task details template

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ToDoList
+from .views import ToDoList, TaskDetails
 
 urlpatterns = [
     path('', ToDoList.as_view(), name='todo'),
+    path('task/<int:pk>', TaskDetails.as_view(), name='task'),
 ]
--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class ToDoList(ListView):
     model = Task
     context_object_name = 'tasks'
+    template_name = 'base/todolist.html'
+
+class TaskDetails(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
