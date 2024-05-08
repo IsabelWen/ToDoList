9bf5eaadf970b6d242cba921aec58832596db6b3
Isy <isy1997@freenet.de>
2024-05-08 13:18:43 +0200

    Create tasks list template

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ToDoList
 
 urlpatterns = [
-    path('', views.todoList, name='todo'),
+    path('', ToDoList.as_view(), name='todo'),
 ]
--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def todoList(request):
-    return HttpResponse('To-Do List')
+class ToDoList(ListView):
+    model = Task
+    context_object_name = 'tasks'
