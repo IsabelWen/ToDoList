396d8912a2083bc7e08fb5dd703bd413f5f1fbe3
Isy <isy1997@freenet.de>
2024-05-10 18:56:02 +0200

    Create edit-task template

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ToDoList, TaskDetails, CreateTask
+from .views import ToDoList, TaskDetails, CreateTask, EditTask, DeleteTask
 
 urlpatterns = [
     path('', ToDoList.as_view(), name='todo'),
     path('task/<int:pk>', TaskDetails.as_view(), name='task'),
     path('create-task/', CreateTask.as_view(), name='create-task'),
+    path('edit-task/<int:pk>/', EditTask.as_view(), name='edit-task'),
+    path('delete-task/<int:pk>/', DeleteTask.as_view, name='delete-task'),
 ]
--- todolist/base/views.py
+++ todolist/base/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Model import
@@ -22,4 +22,14 @@ class CreateTask(CreateView):
     model = Task
     fields  = '__all__'
     success_url = reverse_lazy('todo')
-    #template_name = 
+
+class EditTask(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('todo')
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/delete_task.html'
+    success_url = reverse_lazy('todo')
