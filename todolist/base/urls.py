3fa7a274df57d830faf4b4a0500d0f93dce7725b
Isy <isy1997@freenet.de>
2024-05-10 19:06:26 +0200

    Create delete-task template

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('task/<int:pk>', TaskDetails.as_view(), name='task'),
     path('create-task/', CreateTask.as_view(), name='create-task'),
     path('edit-task/<int:pk>/', EditTask.as_view(), name='edit-task'),
-    path('delete-task/<int:pk>/', DeleteTask.as_view, name='delete-task'),
+    path('delete-task/<int:pk>/', DeleteTask.as_view(), name='delete-task'),
 ]
