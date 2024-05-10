da5214e253a3a5479c6f90a36874511fc9c78711
Isy <isy1997@freenet.de>
2024-05-10 18:41:06 +0200

    Create create-task template

--- todolist/base/urls.py
+++ todolist/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ToDoList, TaskDetails
+from .views import ToDoList, TaskDetails, CreateTask
 
 urlpatterns = [
     path('', ToDoList.as_view(), name='todo'),
     path('task/<int:pk>', TaskDetails.as_view(), name='task'),
+    path('create-task/', CreateTask.as_view(), name='create-task'),
 ]
--- todolist/base/views.py
+++ todolist/base/views.py
@@ -1,6 +1,11 @@
+# Django imports
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+# Model import
 from .models import Task
 
 class ToDoList(ListView):
@@ -12,3 +17,9 @@ class TaskDetails(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class CreateTask(CreateView):
+    model = Task
+    fields  = '__all__'
+    success_url = reverse_lazy('todo')
+    #template_name = 
