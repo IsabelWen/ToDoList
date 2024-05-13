e4ed9c8d189adfa314b38eae5ce1dcf403502db3
Isy <isy1997@freenet.de>
2024-05-13 20:50:02 +0200

    Add function to filter tasks

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -53,6 +53,13 @@ class ToDoList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        # Filter tasks by search input
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        context['search_input'] = search_input
+        
         return context
 
 class TaskDetails(LoginRequiredMixin, DetailView):
